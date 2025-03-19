b241b3922b51b27560e97d6f2cc4ba87d17872fd
HuitaePark <qkrgmlxo3174@gmail.com>
2025-03-19 23:40:14 +0900

    initial commit

---
+++ src/test/java/com/dumbbelling/capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dumbbelling.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
